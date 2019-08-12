df956aa2f35026a9a1318b284dc9bd6f0eab32e4
Kevin Taniguchi <kv.taniguchi@icloud.com>
2019-08-11 21:14:40 -0500

    Initial Commit

---
+++ KTSwiftAlertSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  KTSwiftAlertSwiftUI
+//
+//  Created by Kevin Taniguchi on 8/11/19.
+//  Copyright © 2019 Kevin Taniguchi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
